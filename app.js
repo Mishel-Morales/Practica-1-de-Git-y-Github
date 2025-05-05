1d053f5480418d19e07599657a7885562ef50f3b
Mishel Morales <mmejiam1825@gmail.com>
2025-05-05 16:04:57 -0600

    Segundo cambio realizado en archivo app.js desde rama-b

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log("Hola Mundo")
+
+//Realizando cambios desde rama "rama-b".
+switch(genero){
+    case 'Femenino':
+        console.log('Hola guapa!!')
+        break;
+        case 'Masculino':
+            console.log('Hola')
+        break;
+        default:
+            console.log('Hola!')
+            break;    
+}
