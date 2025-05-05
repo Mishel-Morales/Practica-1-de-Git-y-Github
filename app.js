2a6e4b8f27f7341fdbed534a0f65abcd55277f14
Mishel Morales <mmejiam1825@gmail.com>
2025-05-05 15:04:48 -0600

    Realizando Commit para app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log("Hola Mundo")
+
+import readline from "readline-sync"
+let genero = `femeninino`
+
+switch(genero){
+    case 'Femenino':
+        console.log('Hola guapa!!')
+        break;
+        case 'Masculino':
+            console.log('Hola')
+        break;
+        default:
+            console.log('Hola!')
+            break;    
+}
